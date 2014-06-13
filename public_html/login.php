980e5ef2c4733d13e43e2717b5c9f9b5725e5c37
Jose <n3ggro@gmail.com>
2014-06-13 16:25:16 -0400

    pequeño arreglo en login.php

--- public_html/login.php
+++ public_html/login.php
@@ -1,16 +1,13 @@
 <?php
 	$title = 'Login';
-	$title2 = '<h2>Portal Alumno</h2> ';
 	$content ='
 		<ul>  
 			<form action="../php/login.php" method="POST" >  
-
 				<li>ROL:<br><input type="text" name="rol" id="rol" /></li>
 				<li>Contraseña:<br><input type="password" name="contrasena" id="contrasena" /></li> 
 				<li><input type="submit" /></li>  
 			</form>  
 		</ul>
 	';
-	 
 	include("master.php");  
 ?> 
