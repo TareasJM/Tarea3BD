b50c87cdd4021558e48d383c7ada0928147dbeaf
Jose <n3ggro@gmail.com>
2014-06-13 16:08:24 -0400

    session

--- php/login.php
+++ php/login.php
@@ -2,20 +2,24 @@
 	include("getAlumnos.php");
 
 	function login($user, $pass){
+		session_destroy();
 		$alumnos = getAlumnos();
 		foreach ($alumnos as $alumno) {
 	    	if ($alumno[0] == $user && $alumno[5] == $pass){
-	    		return TRUE;
+	    		return $alumno;
 	    	}
 		}
 		return FALSE;
 	}
-    if (login($_POST[rol], $_POST[contrasena])) {
-        header("Location: ../public_html/index.php");
+
+	$alumno = login($_POST[rol], $_POST[contrasena]); 
+    if ($alumno == FALSE ) {
+        header("Location: ../public_html/login.php");
         die();
     }
     else{
-        header("Location: ../public_html/login.php");
-        die();
+    	session_start();
+		$_SESSION['alumno'] = $alumno;
+        header("Location: ../public_html/index.php");
     }
  ?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,11 @@
 <?php
+	session_start();
 	$title = "Inicio";
-	$content = 'win32_get_last_control_message(aosdifjsodijfsodijfosdijfosijfsdf) win32_get_last_control_message(oid)<br>
-				<a href="ingresarPostulacion.php">Postular</a>';
+	$content = "";
+	if(isset($_SESSION['alumno'])){
+		$content = "hola ". $_SESSION['alumno'][3]."<br>";
+	}
+	$content = $content.'<br><a href="ingresarPostulacion.php">Postular</a>';
 	$sidebar = "ola soy sidebar <br> jiji";
 ?>
 <?php include("master.php"); ?>
